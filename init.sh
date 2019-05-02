19cf44bb06e1a128c0191ac5a537e3bbb087f126
vagrant <vagrant@localhost.localdomain>
2019-05-02 05:45:27 +0000

    update

---
+++ init.sh
@@ -0,0 +1,16 @@
+ethtool -K eth0 lro off
+echo "ethtool -K eth0 lro off"
+ethtool -K eth0 tso off
+echo "ethtool -K eth0 tso off"
+ethtool -K eth0 rx off
+echo "ethtool -K eth0 rx off"
+ethtool -K eth0 tx off
+echo "ethtool -K eth0 tx off"
+ethtool -K eth0 sg off
+echo "ethtool -K eth0 sg off"
+ethtool -K eth0 ufo off
+echo "ethtool -K eth0 ufo off"
+ethtool -K eth0 gso off
+echo "ethtool -K eth0 gso off"
+ethtool -K eth0 gro off
+echo "ethtool -K eth0 gro off"
